24d046a10c059ae94932fc70e2f5ace0e743cb60
Daffna <komkov-1@ya.ru>
2018-12-02 05:12:30 +0700

    portfolio upgrade

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,13 @@ const sides = document.querySelectorAll(".portfolio-side");
 const close = document.createElement('div');
 close.classList.add('portfolio-popup__close');
 
+const headers = document.querySelectorAll(".portfolio-piccontent");
+
 const popup = document.querySelector('.portfolio-choose');
+
+const projectsM = document.querySelector('.portfolio-projects--marketing');
+const projectsF = document.querySelector('.portfolio-projects--frontend');
+
 
 
 btn.addEventListener('click', function () {
@@ -17,12 +23,33 @@ close.addEventListener('click', function (ev) {
     opened.classList.remove("portfolio-sideHover");
     opened.removeChild(close);
     popup.classList.add('hide');
+
+    headers.forEach(function (item) {
+        item.classList.remove('hide')
+    });
+
+    projectsM.classList.add('hide');
+    projectsF.classList.add('hide');
+
     ev.stopPropagation();
 });
 
 sides.forEach(function (node) {
-    node.addEventListener('click', function () {
+    node.addEventListener('click', function (evt) {
         node.classList.add('portfolio-sideHover');
+
+        headers.forEach(function (item) {
+            item.classList.add('hide')
+        });
+
+        if(Array.from(evt.target.classList).includes('portfolio-side--right') || Array.from(evt.target.parentNode.classList).includes('portfolio-side--right')){
+            projectsM.classList.remove('hide');
+        };
+
+        if(Array.from(evt.target.classList).includes('portfolio-side--left') || Array.from(evt.target.parentNode.classList).includes('portfolio-side--left')){
+            projectsF.classList.remove('hide');
+        };
+
         node.appendChild(close);
     });
 });
